31bb091160c3684ca52fb735327e68343d57c9f4
kanthimathi-vr <mathi.v.r.1990@gmail.com>
2025-10-06 23:30:46 +0530

    Update settings.py

--- myproject_ecom/settings.py
+++ myproject_ecom/settings.py
@@ -80,21 +80,11 @@ WSGI_APPLICATION = 'myproject_ecom.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Read DATABASE_URL from environment variables
-        default=config('DATABASE_URL'), 
-        conn_max_age=600 
-    )
-}
-# Fallback for local development if DATABASE_URL isn't set (Optional but helpful)
-if not DATABASES['default']:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
