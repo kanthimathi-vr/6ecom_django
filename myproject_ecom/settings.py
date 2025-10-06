5280b0fdb4940cf18a34b825ebb09d496360378b
kanthimathi-vr <mathi.v.r.1990@gmail.com>
2025-10-06 23:55:22 +0530

    Update settings.py

--- myproject_ecom/settings.py
+++ myproject_ecom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY') 
+SECRET_KEY = 'django-insecure-*wylyt4=hhcynm_xs_vs#yq6yvnmgvi=sf4y!rq2-rm4+ic2ua'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
